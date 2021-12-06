35203d32a21f9fb6d4b73df1b3b01170f19e2e6c
LangdonEcho <86031485+LangdonEcho@users.noreply.github.com>
2021-12-05 19:53:41 -0600

    adding more questions

--- index.js
+++ index.js
@@ -12,18 +12,28 @@ let questions = [
     },
     {
         type:"input",
-        message:"what is the title of your repository",
-        name: "title"
+        message:"write a short overview the project",
+        name: "description"
     },
     {
         type:"input",
-        message:"what is the title of your repository",
-        name: "title"
+        message:"let us know what installations were needed for the project",
+        name: "installation"
     },
     {
         type:"input",
-        message:"what is the title of your repository",
-        name: "title"
+        message:"why did you make this project?",
+        name: "purpose"
+    },
+    {
+        type: "input",
+        message:"What is your email?",
+        name:"email"        
+    },
+    {
+        type: "input",
+         message: "Is there a test included?",
+         name: "tests"
     },
 
 ]
